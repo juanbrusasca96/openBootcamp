cf4da86b97762ca11630c3c5a15d7b87ea5986f0
Juan Brusasca <juanbrusasca96@gmail.com>
2022-06-03 10:19:46 -0300

    Ejercicios tema 3 - Curso de Introducción a la programación

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        suma(1,2,3);
+        Coche coche = new Coche();
+        coche.sumarPuertas();
+        System.out.println(coche.puertas);
+    }
+
+    public static int suma (int a, int b, int c){
+        return a+b+c;
     }
 }
+
+class Coche{
+    public int puertas = 0;
+    public void sumarPuertas(){
+        puertas++;
+    }
+}
+
+
