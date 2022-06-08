f8d024f83072dd133e108f344a66b164f85285ae
Juan Brusasca <juanbrusasca96@gmail.com>
2022-06-08 11:25:10 -0300

    Ejercicios tema 8 - Curso de Introducción a la programación

---
+++ src/ejercicioTema8IntroProgramacion/Main.java
@@ -0,0 +1,43 @@
+package ejercicioTema8IntroProgramacion;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setEdad(25);
+        persona.setNombre("Juan");
+        persona.setTelefono(883355);
+        System.out.println("Edad: " + persona.getEdad());
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Telefono: " + persona.getTelefono());
+    }
+}
+
+class Persona{
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+}
